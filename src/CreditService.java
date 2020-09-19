29768c1612af23af88207fcbcde8d011f385eb84
Nadya Chistyakova <npodtynchenko@list.ru>
2020-09-19 21:02:33 +0300

    edit

--- src/CreditService.java
+++ src/CreditService.java
@@ -1,8 +1,7 @@
 public class CreditService {
-    public double calculate(long sum, int time) {
-        double percent = 0.0999 / 12;
-        double power = Math.pow(1+percent, time);
-        double coefficient = (percent * power)/(power - 1);
+    public double calculate(long sum, int time, double percent) {
+        double power = Math.pow(1 + (percent / 12), time);
+        double coefficient = ((percent / 12) * power) / (power - 1);
         double payment = sum * coefficient;
         return payment;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         CreditService service = new CreditService();
-        int paymentForOneYears = (int) service.calculate(1000000, 12);
+        int paymentForOneYears = (int) service.calculate(1_000_000, 12, 0.0999);
         System.out.println(paymentForOneYears);
 
-        int paymentForTwoYears = (int) service.calculate(1000000, 24);
+        int paymentForTwoYears = (int) service.calculate(1_000_000, 24, 0.0999);
         System.out.println(paymentForTwoYears);
 
-        int paymentForThreeYears = (int) service.calculate(1000000, 36);
+        int paymentForThreeYears = (int) service.calculate(1_000_000, 36, 0.0999);
         System.out.println(paymentForThreeYears);
     }
 }
